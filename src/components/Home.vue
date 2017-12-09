7baaa57d24e2642804e668840f5bfa6e3f409c71
Bruce Lee <supergud3@gmail.com>
2017-12-09 14:24:12 +0800

    update home image size

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="home">
-        <router-link to="/login">
-            <img src="src/assets/logo.png" alt="logo" class="img-responsive">
-            <img src="src/assets/title.png" alt="i-Agriculture" class="img-responsive">
+        <router-link to="/login" class="text-center">
+            <img src="src/assets/logo.png" alt="logo" class="img-responsive logo-img">
+            <img src="src/assets/title.png" alt="i-Agriculture" class="img-responsive title-img">
         </router-link>
     </div>
 </template>
